018c13a8b0337ce1a2d2f2016dcbf9806155166c
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-03-13 14:28:06 -0300

    DNS RPZ Bind project started!

---
+++ src/main/java/br/dev/ferreiras/bloqueioAnatel/BloqueioAnatelApplication.java
@@ -0,0 +1,13 @@
+package br.dev.ferreiras.bloqueioAnatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloqueioAnatelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloqueioAnatelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/ferreiras/bloqueioAnatel/BloqueioAnatelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.ferreiras.bloqueioAnatel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloqueioAnatelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
